8d73b1f820c67a3808d4e7ac4526f1665bfe09dc
Abimbola <craftwordltd@gmail.com>
2020-03-05 05:51:17 +0100

    create site pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/site/home/home.component';
+import { AboutComponent } from './components/site/about/about.component';
+import { ProductComponent } from './components/site/product/product.component';
+import { ContactComponent } from './components/site/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
